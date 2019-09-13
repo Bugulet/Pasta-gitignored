c05a357af4abf9ba7f40a675472fdfc09249d932
Avery Pereira <averyfukawa@gmail.com>
2019-09-13 19:10:57 +0200

    Added interactives.

---
+++ Assets/Scripts/Audio Scripts/PanicMusic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanicMusic : MonoBehaviour
+{
+    [FMODUnity.EventRef] 
+    public string inputSound;
+
+    private bool anxietyIsOver100 = false;
+    
+    
+
+}
