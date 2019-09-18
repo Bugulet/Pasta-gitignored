c44d383a8aba9b1326cb7cf6926c4c7b6ed03703
Eusebiu Buga <bugasebi@yahoo.com>
2019-09-18 13:38:55 +0200

    Placeholder Interaction manager

--- Assets/InteractionManager.cs
+++ Assets/InteractionManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
+
+    [HideInInspector] public int CurrentObjective = 0;
+    [SerializeField] private int MaxObjective = 6;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class InteractionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (CurrentObjective == MaxObjective)
+        {
+            Debug.Log("GOTO END");
+        }
     }
 }
