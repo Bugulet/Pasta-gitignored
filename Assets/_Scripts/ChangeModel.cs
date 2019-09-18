6905aaeb275f5c4bac2a1779206a53cb1220a7f5
Eusebiu Buga <bugasebi@yahoo.com>
2019-09-18 17:07:49 +0200

    Inheritance changes #1

--- Assets/_Scripts/ChangeModel.cs
+++ Assets/_Scripts/ChangeModel.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeModel : MonoBehaviour
+public class ChangeModel : InteractWithObject
 {
 
     [Tooltip("The object that will be spawned when the object will be changed")]
     [SerializeField] private GameObject DifferentObject;
-
-    [Tooltip("The index of the interaction so they happen in order")]
-    [SerializeField] private int InteractionNumber = 0;
-
-    private InteractionManager manager;
+    
 
     private void Start()
     {
@@ -22,8 +18,8 @@ public class ChangeModel : MonoBehaviour
     {
         if (manager.CheckInteraction(InteractionNumber))
         {
-            Instantiate(DifferentObject, transform.position, transform.rotation);
-            Destroy(gameObject);
+                Instantiate(DifferentObject, transform.position, transform.rotation);
+                Destroy(gameObject);
         }
     }
 }
