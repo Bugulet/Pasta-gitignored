192254bdf40cc605e1a084a948f17d45ac7fc51f
Eusebiu Buga <bugasebi@yahoo.com>
2019-09-20 13:24:17 +0200

    Game scene changes + rename script

---
+++ Assets/Rename.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rename : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.name.Contains("Clone"))
+        {
+            gameObject.name = gameObject.name.Substring(0, gameObject.name.Length - 7);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
