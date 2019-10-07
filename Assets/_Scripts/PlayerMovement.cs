29693d2c0cbe24607b631aaff50338ff4bc4b9bd
Avery Pereira <averyfukawa@gmail.com>
2019-10-07 11:21:40 +0200

    updates to movement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private GameObject Player;
 
-    [SerializeField] private float Speed;
+    [SerializeField] public float speed;
 
     private Rigidbody playerRigidbody;
 
@@ -35,6 +35,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+            float moveHorizontal = Input.GetAxis ("Horizontal")*speed;
+            float moveVertical = Input.GetAxis ("Vertical")*speed;
+            moveHorizontal *= Time.deltaTime;
+            moveVertical *= Time.deltaTime;
+
+            transform.Translate(moveHorizontal, 0.0f, moveVertical);
+    }
+    
+        /*
         if (Player.GetComponentInChildren<Camera>().enabled)
         {
             float speedMultiplier = Speed * Time.deltaTime;
@@ -58,6 +67,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 playerRigidbody.AddForce(-speedMultiplier * Player.transform.forward);
             }
+            
         }
-    }
+        */
 }
