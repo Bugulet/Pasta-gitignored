3dcdff90064893cef8f17ab0631043c9240dc71c
averyfukawa <averyfukawa@users.noreply.github.com>
2019-10-11 17:32:40 +0200

    one prefab fixed, dupe player position float inaccuracies fixed

---
+++ Assets/_Scripts/CopyPlayerPos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPlayerPos : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPos;
+    [SerializeField] private GameObject dupePlayer;
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        dupePlayer.transform.position = new Vector3(playerPos.transform.position.x, playerPos.transform.position.y,
+            (playerPos.transform.position.z + 100));
+    }
+}
