59ecb48d7346cc84ee543e325e6b8f689e76e674
averyfukawa <averyfukawa@users.noreply.github.com>
2019-11-04 14:09:38 +0100

    Forgot fire room rotation needed to freeze when pausing

---
+++ Assets/_Scripts/CopyCamera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCam;
+    [SerializeField] private GameObject fireCam;
+    private void Start()
+    {
+        if (playerCam == null || fireCam == null)
+        {
+            throw new Exception("Camera missing from " + gameObject.name);
+        }
+    }
+
+    private void Update()
+    {
+        float camX = playerCam.transform.eulerAngles.x;
+        float camY = playerCam.transform.eulerAngles.y;
+        float camZ = playerCam.transform.eulerAngles.z;
+        fireCam.transform.eulerAngles = new Vector3(camX, camY, camZ);
+    }
+}
